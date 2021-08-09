9feb64e4b6c20d3f0a1d34596a47b50d92414172
Raphael Monte Santos <raphael.santos@hotmart.com>
2021-08-09 19:37:08 -0300

    test: implement Controller protocols, helpers and errors

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from '../errors/missing-param-error'
 import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
@@ -6,11 +7,37 @@ describe('SignUp Controller', () => {
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
+        password: 'any_password'
       }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(new MissingParamError('name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(new MissingParamError('password'))
   })
 })
