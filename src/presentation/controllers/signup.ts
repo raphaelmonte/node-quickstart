d121e111f8274ac604cd9aefb2a7b8c2cb1c66ca
Raphael Monte Santos <raphael.santos@hotmart.com>
2021-08-09 19:36:30 -0300

    refactor: implement Controller protocols, helpers and errors in SignUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,16 @@
-export class SignUpController {
-  handle (httpRequest: any): any {
-    return {
-      statusCode: 400
+import { HttpRequest, HttpResponse } from '../protocols/http'
+import { Controller } from '../protocols/controller'
+import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
+
+export class SignUpController implements Controller {
+  handle (httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email', 'password']
+    for (const field of requiredFields) {
+      if (!httpRequest?.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
+    return { statusCode: 200 }
   }
 }
